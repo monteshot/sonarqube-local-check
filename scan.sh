09684b7e979b49d1435576ffbb7dca557845bf7e
monteshot <faqreg@gmail.com>
2024-11-04 12:43:30 +0200

    feat: add .env support for scan script and update .gitignore
    Update .gitignore to exclude .env file for security
    Modify scan.sh to load environment variables from .env if present
    Create .env.example to provide example values for required environment variables:
    YOUR_CACHE_DIR: path to SonarQube cache directory
    YOUR_REPO: repository directory
    SONARQUBE_URL: SonarQube server URL
    SONAR_TOKEN: SonarQube authentication token
    PROJECT_KEY: SonarQube project key

--- scan.sh
+++ scan.sh
@@ -1,9 +1,8 @@
 docker compose up -d
-YOUR_CACHE_DIR=/tmp/sonar/cache/
-YOUR_REPO=/home/monteshot/ApacheSites/pointspay.test/content/
-SONARQUBE_URL="sonarqube:9000"
-SONAR_TOKEN="sqp_3fb3dbeea26d6dcf01444c7b1193d5305cddc147"
-PROJECT_KEY="pointspay"
+
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
 
 docker run \
     --rm \
@@ -12,5 +11,9 @@ docker run \
     -v ${YOUR_CACHE_DIR}:/opt/sonar-scanner/.sonar/cache \
     -e SONAR_HOST_URL="http://${SONARQUBE_URL}" \
     -e SONAR_TOKEN="${SONAR_TOKEN}" \
-    -e EXTRA_ARGS="-Dsonar.projectKey=${PROJECT_KEY} -Dsonar.qualitygate.wait=true  -Dsonar.exclusions=app/code/Hyva/CmsTailwindJit/view/adminhtml/web/*,app/code/Pointspay/Pointspay/Test  -Dsonar.inclusions=app/code/Perspective/**,app/code/CodeCustom/**,app/design/**,app/code/Pointspay/** "\
+    -e EXTRA_ARGS="-Dsonar.projectKey=${PROJECT_KEY} \
+    -Dsonar.qualitygate.wait=true \
+    -Dsonar.exclusions=app/code/Hyva/CmsTailwindJit/view/adminhtml/web/*,app/code/Pointspay/Pointspay/Test \
+    -Dsonar.inclusions=app/code/Perspective/**,app/code/CodeCustom/**,app/design/**,app/code/Pointspay/** \
+     "\
     sonarsource/sonarqube-scan
